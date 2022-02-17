efbcb1d4c55ce2cad4df81cf0c58b0b9dc52c6e2
L-TangC <87227806+L-TangC@users.noreply.github.com>
2022-02-17 09:52:39 +0800

    delete old js
    delete old js

--- js/all.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".add_page_btn").click(function(){
-        $(".add_page,.blur_bg").show();
-    });
-    $(".done_btn,.close_btn").click(function(){
-        $(".add_page,.blur_bg").hide();
-    });
-});
